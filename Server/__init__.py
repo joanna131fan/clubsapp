9e062a9d6e115a31934ff7b163e3d896a9d7841a
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 20:05:10 -0700

    import flask_assets

--- Server/__init__.py
+++ Server/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_assets import Environment, Bundle
 from clubsapp.config import Config
 
 db = SQLAlchemy()
