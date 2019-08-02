b46cf9ce55257abd8d524d2813cd1e7fc2f0c2ee
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 20:32:25 -0700

    Create utils.py

---
+++ Server/users/utils.py
@@ -0,0 +1,29 @@
+
+import os
+import secrets
+import hashlib
+from flask import url_for, current_app
+from flask_mail import Message
+from clubsapp import mail
+
+
+def hash_pw(password, salt=None):
+	'''Hashes pwd. Output is of form `salt, hash`.'''
+	if not salt:
+		salt = secrets.token_hex(8)
+	pre_hash = (password + salt).encode()
+	hash = hashlib.sha256(pre_hash).hex_digest()
+	return salt, hash
+	
+
+def send_reset_email(user):
+	token = user.get_reset_token()
+	msg = Message('Password Reset Request', 
+		sender='',
+		recipients=[user.email])
+		
+	msg.body = '''To reset your password, visit the following link:
+...
+If you did not make this request, please ignore this email and no changes will be made.
+'''
+	mail.seng(msg)
