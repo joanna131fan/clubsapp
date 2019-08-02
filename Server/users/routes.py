dc4cf2a7a70d3356ed564b15ddf8f17ca56ec6b0
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 20:14:35 -0700

    Create routes.py

---
+++ Server/users/routes.py
@@ -0,0 +1,27 @@
+
+from flask import Blueprint, render_template, redirect, request, url_for, flash
+from flask_login import login_user, current_user, logout_user, login_required
+from clubsapp import db
+from clubsapp.models import User
+from clubsapp.users.forms import RegistrationForm, LoginForm
+from flaskblog.users.utils import hash_pw
+
+
+users = Blueprint('users', __name__)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('main.home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+      return redirect(url_for('main.home'))
+    return render_template('login.html', title='Login', form=form)
