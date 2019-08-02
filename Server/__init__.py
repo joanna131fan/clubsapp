c6147607de5d33c4280042a8b818580686990c2d
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 20:03:47 -0700

    Update __init__.py

--- Server/__init__.py
+++ Server/__init__.py
@@ -11,6 +11,13 @@ login_manager.login_message_category = 'info'
 mail = Mail()
 
 
+# Make sure this is a good way to deal with this
+js = Bundle('main.js', 'bootstrap.js',output='gen/all.js')
+
+assets = Environment(app)
+assets.register('all.js', js)
+
+
 def create_app(config_class=Config):
   app = Flask(__name__)
   app.config.from_object(config_class)
