756c429611ec6586a545ab6984a4c59a62dcee75
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 21:41:40 -0700

    Update routes.py

--- clubsapp/users/routes.py
+++ clubsapp/users/routes.py
@@ -13,18 +13,17 @@ users = Blueprint('users', __name__)
 @users.route('/register', methods=['GET', 'POST'])
 def register():
 	if current_user.is_authenticated:
-    	return redirect(url_for('main.home'))
+		return redirect(url_for('main.home'))
     
 	form = RegistrationForm()
 	if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(firstname=form.firstname.data, lastname=form.lastname.data,
-       	password=hashed_password)
+    	hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    	user = User(firstname=form.firstname.data, lastname=form.lastname.data, password=hashed_password)
 		db.session.add(user)
 		db.session.commit()
-        flash('Your account has been created, you are now able to log in!', 'success')
-        return redirect(url_for('users.login'))
-    return render_template('register.html', title='Register', form=form)
+		flash('Your account has been created, you are now able to log in!', 'success')
+		return redirect(url_for('users.login'))
+	return render_template('register.html', title='Register', form=form)
 
 
 @users.route('/login', methods=['GET', 'POST'])
