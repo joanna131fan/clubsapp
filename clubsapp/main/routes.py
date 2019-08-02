f7bff1fbd65d8765c77a75930a5fd8bf26de3330
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 21:25:12 -0700

    Update routes.py

--- clubsapp/main/routes.py
+++ clubsapp/main/routes.py
@@ -5,12 +5,12 @@ from flask import Blueprint, render_template, request
 main = Blueprint('main', __name__)
 
 
-@app.route("/")
-@app.route("/home")
+@main.route("/")
+@main.route("/home")
 def home():
     return render_template('home.html')
 
 
-@app.route("/about")
+@main.route("/about")
 def about():
     return render_template('about.html', title='About')
