acedb97b6e0ac44cb0071130418516b30b20df56
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 20:43:26 -0700

    Update __init__.py

--- Server/__init__.py
+++ Server/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_assets import Environment, Bundle
 from clubsapp.config import Config
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
@@ -20,20 +22,20 @@ assets.register('all.js', js)
 
 
 def create_app(config_class=Config):
-  app = Flask(__name__)
-  app.config.from_object(config_class)
+	app = Flask(__name__)
+	app.config.from_object(config_class)
   
-  db.init_app(app)
-  login_manager.init_app(app)
-  mail.init_app(app)
+	db.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
   
-  # Import Blueprints
-  from clubsapp.main.routes import main
-  from clubsapp.users.routes import users
-  from clubsapp.clubs.routes import clubs
-  app.register_blueprint(main)
-  app.register_blueprint(users)
-  app.register_blueprint(clubs)
+	# Import Blueprints
+	from clubsapp.main.routes import main
+	from clubsapp.users.routes import users
+	from clubsapp.clubs.routes import clubs
+	app.register_blueprint(main)
+	app.register_blueprint(users)
+	app.register_blueprint(clubs)
 
-  return app
+	return app
   
