d0290ca6dd14454ca469457903d0bac5502b8fd9
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 20:06:46 -0700

    Create routes.py

---
+++ Server/main/routes.py
@@ -0,0 +1,13 @@
+
+from flask import Blueprint, render_template, request
+from clubserver import app
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
