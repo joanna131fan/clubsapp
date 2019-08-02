5fb59f27e2fdac6473dc3b65248773aa827b29c6
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 19:42:02 -0700

    Create __init__.py

---
+++ Server/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from Server.config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+def create_app(config_class=Config):
+  app = Flask(__name__)
+  app.config.from_object(config_class)
+  
+  db.init_app(app)
+  login_manager.init_app(app)
+  
+  # Import Blueprints
+  
+  
+  return app
+  
