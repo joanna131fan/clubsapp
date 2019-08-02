08223efad6c5853bad78b0be4807484dfd0f90f2
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-01 20:51:45 -0700

    Update routes.py

--- Server/users/routes.py
+++ Server/users/routes.py
@@ -17,14 +17,33 @@ def register():
     
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.firstname.data}' {form.lastname.data}, 'success')
-        return redirect(url_for('main.home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(firstname=form.firstname.data, lastname=form.lastname.data,
+       		password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		
+        flash('Your account has been created, you are now able to log in!', 'success')
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+	
     form = LoginForm()
     if form.validate_on_submit():
-      return redirect(url_for('main.home'))
+		user = User.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			# remembers if login was prompted from a different page
+			next_page = request.args.get('next')
+    		return redirect(next_page) if next_page else redirect(url_for('main.home'))
+		else:
+			flash('Login unsucessful. Please check your email and password!', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+
