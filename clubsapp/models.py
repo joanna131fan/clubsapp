78fa3d28fa516d3aba60bbd44acaf0b6746d8e1e
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-03 12:16:54 -0700

    Update models.py

--- clubsapp/models.py
+++ clubsapp/models.py
@@ -6,7 +6,7 @@ from clubsapp import db, login_manager
 
 
 user_club_assoc_table = db.Table('user_club_assoc_table',
-	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),																						db.Column('club_id', db.Integer, db.ForeignKey('club.id')))
+	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),									db.Column('club_id', db.Integer, db.ForeignKey('club.id')))
 
 
 class User(db.Model, UserMixin):
@@ -22,6 +22,8 @@ class User(db.Model, UserMixin):
 class Club(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100), unique=True, nullable=False)
+	advisor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	advisor = db.relatioship('User', back_populates='clubs')
 	members = db.relationship('User', secondary=user_club_assoc_table)
 	
 	
