eed6961a5050162eee973c5743fe6d47b915ae20
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-03 12:52:30 -0700

    Update models.py

--- clubsapp/models.py
+++ clubsapp/models.py
@@ -11,6 +11,12 @@ user_club_assoc_table = db.Table('user_club_assoc_table',
 roles = db.relationship('Role', secondary='user_roles',
                 backref=db.backref('users', lazy='dynamic'))
 
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	firstname = db.Column(db.String(15), nullable=False)
