5030d97bbf2dce364a5f18add5ee6e6d6a1490a5
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-03 12:46:31 -0700

    Update models.py

--- clubsapp/models.py
+++ clubsapp/models.py
@@ -8,6 +8,8 @@ from clubsapp import db, login_manager
 user_club_assoc_table = db.Table('user_club_assoc_table',
 	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),									db.Column('club_id', db.Integer, db.ForeignKey('club.id')))
 
+roles = db.relationship('Role', secondary='user_roles',
+                backref=db.backref('users', lazy='dynamic'))
 
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
@@ -19,14 +21,6 @@ class User(db.Model, UserMixin):
 	roles = db.relationship('Role', secondary='user_roles', backref=db.backref('users', lazy='dynamic'))
 
 
-class Club(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(100), unique=True, nullable=False)
-	advisor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-	advisor = db.relationship('User', back_populates='clubs')
-	members = db.relationship('User', secondary=user_club_assoc_table)
-	
-	
 class Role(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50), nullable=False, unique=True)
@@ -36,4 +30,13 @@ class UserRoles(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
 	role_id = db.Column(db.Integer, db.ForeignKey('role.id', ondelete='CASCADE'))
+
+
+class Club(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), unique=True, nullable=False)
+	advisor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	advisor = db.relationship('User', back_populates='clubs')
+	members = db.relationship('User', secondary=user_club_assoc_table)
+	
 
