041e5bbc6318074fdf95115b4024d882a8672b02
paul <paulmorenkov@gmail.com>
2019-08-03 04:46:39 +0000

    add roles and force login

--- clubsapp/main/routes.py
+++ clubsapp/main/routes.py
@@ -1,5 +1,6 @@
 
 from flask import Blueprint, render_template, request
+from flask_login import login_required
 
 
 main = Blueprint('main', __name__)
@@ -7,10 +8,11 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 @main.route("/home")
+@login_required
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 
 @main.route("/about")
 def about():
-    return render_template('about.html', title='About')
+	return render_template('about.html', title='About')
--- clubsapp/models.py
+++ clubsapp/models.py
@@ -1,28 +1,36 @@
-
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin
 from clubsapp import db, login_manager
 
-
-user_club_assoc_table = db.Table('user_club_assoc_table', 
-  db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-  db.Column('club_id', db.Integer, db.ForeignKey('club.id'))
-
-)
+user_club_assoc_table = db.Table('user_club_assoc_table',
+	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),																						db.Column('club_id', db.Integer, db.ForeignKey('club.id')))
 
 
 class User(db.Model, UserMixin):
-  id = db.Column(db.Integer, primary_key=True)
-  firstname = db.Column(db.String(15), nullable=False)
-  lastname = db.Column(db.String(15), nullable=False)
-  email = db.Column(db.String(60), unique=True, nullable=False)
-  password = db.Column(db.String(60), nullable=False)
-  clubs = db.relationship('Club', secondary=user_club_assoc_table)
+	id = db.Column(db.Integer, primary_key=True)
+	firstname = db.Column(db.String(15), nullable=False)
+	lastname = db.Column(db.String(15), nullable=False)
+	email = db.Column(db.String(60), unique=True, nullable=False)
+	password = db.Column(db.String(60), nullable=False)
+	clubs = db.relationship('Club', secondary=user_club_assoc_table)
+	roles = db.relationship('Role', secondary='user_roles', backref=db.backref('users', lazy='dynamic'))
 
-  
+
 class Club(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100), unique=True, nullable=False)
-  members = db.relationship('User', secondary=user_club_assoc_table)
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), unique=True, nullable=False)
+	members = db.relationship('User', secondary=user_club_assoc_table)
+	
+	
+class Role(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), nullable=False, unique=True)
+	
+	
+class UserRoles(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+	role_id = db.Column(db.Integer, db.ForeignKey('role.id', ondelete='CASCADE'))
+
