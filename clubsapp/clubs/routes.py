cf1ba7c4fb0ab33795aacbc5650dcd2489c89051
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-12 23:02:12 -0700

    Update routes.py

--- clubsapp/clubs/routes.py
+++ clubsapp/clubs/routes.py
@@ -20,13 +20,13 @@ def user_clubs(user_id):
 		advisor = User.query.filter_by(email=form.email.data)
 		if not advisor or advisor.role != ROLES['teacher']:
 			flash('That email does not belong to an advisor, or does not exist at all!', 'danger')
-			return
+			return render_template('user_clubs.html', clubs=clubs, user=user, form=form)
 		club = Club(name=form.name.data)
 		db.session.add(club)
 		db.session.commit()
 		club.members.append(advisor)
 		flash('Your club has been created!', 'success')
-		return redirect('user_clubs.html', clubs=clubs, user=user, form=form)
+		return render_template('user_clubs.html', clubs=clubs, user=user, form=form)
 	return render_template('user_clubs.html', clubs=clubs, user=user, form=form)
 
 
