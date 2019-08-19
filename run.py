4d8d2c09bf3ff02e6c8ecdd294dc4bdf343a4983
Paul Morenkov <paulmorenkov@gmail.com>
2019-08-19 23:22:20 +0000

    add auto toggle debug

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from clubsapp import create_app
 
 
 if __name__ == '__main__':
-  app = create_app()
-  
-  #TODO: check OS type and set debug to True
-  app.run()
+	app = create_app()	
+	#TODO: check OS type and set debug to True
+	debug = False if app.config['DEVICE_TYPE'] == 'iPad' else True
+	app.run(debug=debug)
