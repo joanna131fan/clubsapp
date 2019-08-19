64de03e0bb3106714903c399b5dbb35d7a81fad7
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-19 16:52:59 -0700

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,18 @@
 
 from clubsapp.models import User
+from clubsapp.utils import ROLES
 
 
 def test_new_user():
-	pass
+	"""
+	GIVEN a User model
+	WHEN a new User is created
+	THEN check the firstname, lastname, email, password, roles, and members
+	"""
+	user = User('Bob', 'White', 'BobWhite@gmail.com', 'password123!')
+	assert user.firstname == 'Bob'
+	assert user.lastname == 'White'
+	assert user.email = 'BobWhite@gmail.com'
+	assert user.password == 'password123!' # Check hashing later
+	assert user.role == ROLES['STUDENT']
+	assert user.members = []
