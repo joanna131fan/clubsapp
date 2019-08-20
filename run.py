915708a67962f6110fcce8e8ff0ad852d165d62b
TheGreatSquid <50186460+TheGreatSquid@users.noreply.github.com>
2019-08-19 22:27:48 -0700

    reformat auto-debug mode

--- run.py
+++ run.py
@@ -4,5 +4,8 @@ from clubsapp import create_app
 if __name__ == '__main__':
 	app = create_app()	
 	#TODO: check OS type and set debug to True
-	debug = False if app.config['DEVICE_TYPE'] == 'iPad' else True
+	debug = False
+	if app.config['DEVICE_TYPE'] == 'MacBook':
+		debug = True
+	
 	app.run(debug=debug)
