cb4b0d43e55f72da53f853e27deea82056cb4d7d
Paul Morenkov <paulmorenkov@gmail.com>
2019-08-21 23:16:59 +0000

    change config

--- clubsapp/config.py
+++ clubsapp/config.py
@@ -8,8 +8,8 @@ except ModuleNotFoundError as e:
 
 
 class Config:
-	SECRET_KEY = target['BLOG_SECRET_KEY'] or '97df9c8029f9e716e18088cb1db30e23'
-	SQLALCHEMY_DATABASE_URI = target['BLOG_DB_URI']
+	SECRET_KEY = target['CLUB_SECRET_KEY'] or '97df9c8029f9e716e18088cb1db30e23'
+	SQLALCHEMY_DATABASE_URI = target['CLUB_DB_URI']
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
